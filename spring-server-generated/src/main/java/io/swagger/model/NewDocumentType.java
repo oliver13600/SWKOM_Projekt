00843c744eb92bff454c5b2fa15d7e283dd6179f
MrGreenMeme <lorenzflich@gmail.com>
2023-09-25 14:35:05 +0200

    Swagger-OpenAPI Setup and Implementation

---
+++ spring-server-generated/src/main/java/io/swagger/model/NewDocumentType.java
@@ -0,0 +1,151 @@
+package io.swagger.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.v3.oas.annotations.media.Schema;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * NewDocumentType
+ */
+@Validated
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2023-09-25T10:52:03.827974241Z[GMT]")
+
+
+public class NewDocumentType   {
+  @JsonProperty("name")
+  private String name = null;
+
+  @JsonProperty("match")
+  private String match = null;
+
+  @JsonProperty("matching_algorithm")
+  private Long matchingAlgorithm = null;
+
+  @JsonProperty("is_insensitive")
+  private Boolean isInsensitive = null;
+
+  public NewDocumentType name(String name) {
+    this.name = name;
+    return this;
+  }
+
+  /**
+   * Get name
+   * @return name
+   **/
+  @Schema(description = "")
+  
+    public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public NewDocumentType match(String match) {
+    this.match = match;
+    return this;
+  }
+
+  /**
+   * Get match
+   * @return match
+   **/
+  @Schema(description = "")
+  
+    public String getMatch() {
+    return match;
+  }
+
+  public void setMatch(String match) {
+    this.match = match;
+  }
+
+  public NewDocumentType matchingAlgorithm(Long matchingAlgorithm) {
+    this.matchingAlgorithm = matchingAlgorithm;
+    return this;
+  }
+
+  /**
+   * Get matchingAlgorithm
+   * @return matchingAlgorithm
+   **/
+  @Schema(description = "")
+  
+    public Long getMatchingAlgorithm() {
+    return matchingAlgorithm;
+  }
+
+  public void setMatchingAlgorithm(Long matchingAlgorithm) {
+    this.matchingAlgorithm = matchingAlgorithm;
+  }
+
+  public NewDocumentType isInsensitive(Boolean isInsensitive) {
+    this.isInsensitive = isInsensitive;
+    return this;
+  }
+
+  /**
+   * Get isInsensitive
+   * @return isInsensitive
+   **/
+  @Schema(description = "")
+  
+    public Boolean isIsInsensitive() {
+    return isInsensitive;
+  }
+
+  public void setIsInsensitive(Boolean isInsensitive) {
+    this.isInsensitive = isInsensitive;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    NewDocumentType newDocumentType = (NewDocumentType) o;
+    return Objects.equals(this.name, newDocumentType.name) &&
+        Objects.equals(this.match, newDocumentType.match) &&
+        Objects.equals(this.matchingAlgorithm, newDocumentType.matchingAlgorithm) &&
+        Objects.equals(this.isInsensitive, newDocumentType.isInsensitive);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name, match, matchingAlgorithm, isInsensitive);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class NewDocumentType {\n");
+    
+    sb.append("    name: ").append(toIndentedString(name)).append("\n");
+    sb.append("    match: ").append(toIndentedString(match)).append("\n");
+    sb.append("    matchingAlgorithm: ").append(toIndentedString(matchingAlgorithm)).append("\n");
+    sb.append("    isInsensitive: ").append(toIndentedString(isInsensitive)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
